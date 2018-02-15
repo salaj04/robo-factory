b76fd92eb4a5b7f1be02a665337c116f9fa76273
Salaj Mithbawkar <salaj.mithbawkar@cgx.com.au>
2018-02-15 15:26:07 +1100

    Installations

--- app/controllers/robot_controller.rb
+++ app/controllers/robot_controller.rb
@@ -1,2 +1,16 @@
 class RobotController < ApplicationController
+
+  def index
+  end
+
+  def create
+    create = CreatesRobots.create!(create_params)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
